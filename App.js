5e8eb5211aea3a8013baefa0b4456976d8ccf361
José Mauricio <lmauricio-2010@hotmail.com>
2021-04-26 22:15:37 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,16 +1,58 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View style={styles.container}>
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3}>
+        <View style={styles.subbox} />
+        <View style={styles.subbox} />
+        <View style={styles.subbox} />
+      </View>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    // flexDirection: 'row',
+    // Primeiro eixo .. Y
+    justifyContent: 'center',
+    // justifyContent: 'flex-start',
+    // justifyContent: 'flex-end',
+    // justifyContent: 'space-around',
+    // justifyContent: 'space-between',
+    // Segundo eixo .. X
+    // alignItems: 'center',
+    // alignItems: 'flex-start',
+    // alignItems: 'flex-end',
+    alignItems: 'stretch',
+  },
+  box1: {
+    // padding: 40,
+    flex: 1,
+    backgroundColor: 'steelblue',
+    margin: 4
+  },
+  box2: {
+    flex: 2,
+    backgroundColor: 'steelblue',
+    margin: 4
+  },
+  box3: {
+    height: 80,
+    flexDirection: 'row',
+    backgroundColor: 'steelblue',
+    margin: 4
+  },
+  subbox: {
+    flex: 1,
+    margin: 4,
+    backgroundColor: 'yellow'
+  }
+})
